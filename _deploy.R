d18804126acb6af1eff55ba3a9523f104afc6cbd
Matt Landis <matt.landis@rsginc.com>
2020-12-01 21:48:38 -0500

    Fixing deploy script

--- _deploy.R
+++ _deploy.R
@@ -8,10 +8,18 @@ support_files = c(
   'data/tnc_bayarea_hh_labeled.rds'
 )
 
-app_file = '06_shiny_flex_crosstalk.Rmd'
+app_file = '05_shiny_flex_crosstalk.Rmd'
 
 deployApp(
   appFiles = c(support_files, app_file),
-  appTitle = paste0('rted_demo_', str_replace(app_file, '[.](Rmd|R)', '')),
+  appTitle = paste0('landis_demo_', str_replace(app_file, '[.](Rmd|R)', '')),
+  forceUpdate=TRUE
+)
+
+app_file = '06_shiny_filter_map_from_table.R'
+
+deployApp(
+  appFiles = c(support_files, app_file),
+  appTitle = paste0('landis_demo_', str_replace(app_file, '[.](Rmd|R)', '')),
   forceUpdate=TRUE
 )
