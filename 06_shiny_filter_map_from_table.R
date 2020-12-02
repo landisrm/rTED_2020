17f5da7e8d8faee70b8358187b1e8c983a03c235
Matt Landis <matt.landis@rsginc.com>
2020-12-01 22:25:33 -0500

    Updated zoom level in 06

--- 06_shiny_filter_map_from_table.R
+++ 06_shiny_filter_map_from_table.R
@@ -135,7 +135,7 @@ server <- function(session, input, output) {
       setView(
         lat = map_center['lat'],
         lng = map_center['lon'],
-        zoom = 9
+        zoom = 8
       ) # %>%
     # addLegend(pal=pal, values=~income_aggregate, opacity=0.5)
     
